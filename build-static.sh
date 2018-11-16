f5337216335c3cc00c79b4e6613237d53b7dea62
Pragyan Tripathi <pntripathi9417@gmail.com>
2018-11-16 17:38:38 +0530

    fix(nerdsden): fix issue

--- build-static.sh
+++ build-static.sh
@@ -12,15 +12,15 @@ wget --convert-links --page-requisites --no-parent --directory-prefix static --n
 wget --convert-links --page-requisites --no-parent --directory-prefix static --no-host-directories --restrict-file-name=unix http://localhost:2368/sitemap-tags.xml
 
 # Replace urls that were missed by buster
-find static/* -name "robots.txt" -type f -exec sed -i '' 's#http://localhost:2368#http://nerds-den.com#g' {} \;
-find static/* -name "*.xsl"  -type f -exec sed -i '' 's#http://localhost:2368#http://nerds-den.com#g' {} \;
+find static/* -name "robots.txt" -type f -exec sed -i '' 's#http://localhost:2368#https://nerds-den.com#g' {} \;
+find static/* -name "*.xsl"  -type f -exec sed -i '' 's#http://localhost:2368#https://nerds-den.com#g' {} \;
 find static/* -name "*.xml"  -type f -exec sed -i '' 's#href="//localhost:2368#href="//nerds-den.com#g' {} \;
-find static/* -name "*.xml"  -type f -exec sed -i '' 's#loc>http://localhost:2368#loc>http://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#u=http://localhost:2368#u=http://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#url=http://localhost:2368#url=http://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#href="http://localhost:2368#href="http://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#src="http://localhost:2368#src="http://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#link>http://localhost:2368#link>http://nerds-den.com#g' {} \;
+find static/* -name "*.xml"  -type f -exec sed -i '' 's#loc>http://localhost:2368#loc>https://nerds-den.com#g' {} \;
+find static/* -name "*.html" -type f -exec sed -i '' 's#u=http://localhost:2368#u=https://nerds-den.com#g' {} \;
+find static/* -name "*.html" -type f -exec sed -i '' 's#url=http://localhost:2368#url=https://nerds-den.com#g' {} \;
+find static/* -name "*.html" -type f -exec sed -i '' 's#href="http://localhost:2368#href="https://nerds-den.com#g' {} \;
+find static/* -name "*.html" -type f -exec sed -i '' 's#src="http://localhost:2368#src="https://nerds-den.com#g' {} \;
+find static/* -name "*.html" -type f -exec sed -i '' 's#link>http://localhost:2368#link>https://nerds-den.com#g' {} \;
 
 # Add CNAME file for github pages
 buster add-domain nerds-den.com
