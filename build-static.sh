1985bf32adbfec189c9b097dcf979ac5e3e890ca
Pragyan Tripathi <pntripathi9417@gmail.com>
2018-11-16 17:40:51 +0530

    fix(nerdsden): fix localhost url in index.html

--- build-static.sh
+++ build-static.sh
@@ -19,6 +19,7 @@ find static/* -name "*.xml"  -type f -exec sed -i '' 's#loc>http://localhost:236
 find static/* -name "*.html" -type f -exec sed -i '' 's#u=http://localhost:2368#u=https://nerds-den.com#g' {} \;
 find static/* -name "*.html" -type f -exec sed -i '' 's#url=http://localhost:2368#url=https://nerds-den.com#g' {} \;
 find static/* -name "*.html" -type f -exec sed -i '' 's#href="http://localhost:2368#href="https://nerds-den.com#g' {} \;
+find static/* -name "*.html" -type f -exec sed -i '' 's#href="http://127.0.0.1:2368#href="https://nerds-den.com#g' {} \;
 find static/* -name "*.html" -type f -exec sed -i '' 's#src="http://localhost:2368#src="https://nerds-den.com#g' {} \;
 find static/* -name "*.html" -type f -exec sed -i '' 's#link>http://localhost:2368#link>https://nerds-den.com#g' {} \;
 
