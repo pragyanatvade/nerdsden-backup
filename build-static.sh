687efc69703173e9cc3afc069c1541804070ad81
Pragyan Tripathi <pntripathi9417@gmail.com>
2018-11-17 21:46:38 +0600

    Blog update at 2018-11-17 15:46:38

--- build-static.sh
+++ build-static.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Generate static files with buster
-buster generate --domain=http://127.0.0.1:2368
+buster generate --domain=http://127.0.0.1:2368 --dir docs
 
 # Copy sitemap files
 wget --convert-links --page-requisites --no-parent --directory-prefix static --no-host-directories --restrict-file-name=unix http://127.0.0.1:2368/sitemap.xsl
@@ -12,24 +12,24 @@ wget --convert-links --page-requisites --no-parent --directory-prefix static --n
 wget --convert-links --page-requisites --no-parent --directory-prefix static --no-host-directories --restrict-file-name=unix http://localhost:2368/sitemap-tags.xml
 
 # Replace urls that were missed by buster
-find static/* -name "robots.txt" -type f -exec sed -i '' 's#http://localhost:2368#https://nerds-den.com#g' {} \;
-find static/* -name "*.xsl"  -type f -exec sed -i '' 's#http://localhost:2368#https://nerds-den.com#g' {} \;
-find static/* -name "*.xml"  -type f -exec sed -i '' 's#href="//localhost:2368#href="//nerds-den.com#g' {} \;
-find static/* -name "*.xml"  -type f -exec sed -i '' 's#loc>http://localhost:2368#loc>https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#u=http://localhost:2368#u=https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#url=http://localhost:2368#url=https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#href="http://localhost:2368#href="https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#href="http://127.0.0.1:2368#href="https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#src="http://localhost:2368#src="https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#link>http://localhost:2368#link>https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#http://localhost:2368#https://nerds-den.com#g' {} \;
+find docs/* -name "robots.txt" -type f -exec sed -i '' 's#http://localhost:2368#https://nerds-den.com#g' {} \;
+find docs/* -name "*.xsl"  -type f -exec sed -i '' 's#http://localhost:2368#https://nerds-den.com#g' {} \;
+find docs/* -name "*.xml"  -type f -exec sed -i '' 's#href="//localhost:2368#href="//nerds-den.com#g' {} \;
+find docs/* -name "*.xml"  -type f -exec sed -i '' 's#loc>http://localhost:2368#loc>https://nerds-den.com#g' {} \;
+find docs/* -name "*.html" -type f -exec sed -i '' 's#u=http://localhost:2368#u=https://nerds-den.com#g' {} \;
+find docs/* -name "*.html" -type f -exec sed -i '' 's#url=http://localhost:2368#url=https://nerds-den.com#g' {} \;
+find docs/* -name "*.html" -type f -exec sed -i '' 's#href="http://localhost:2368#href="https://nerds-den.com#g' {} \;
+find docs/* -name "*.html" -type f -exec sed -i '' 's#href="http://127.0.0.1:2368#href="https://nerds-den.com#g' {} \;
+find docs/* -name "*.html" -type f -exec sed -i '' 's#src="http://localhost:2368#src="https://nerds-den.com#g' {} \;
+find docs/* -name "*.html" -type f -exec sed -i '' 's#link>http://localhost:2368#link>https://nerds-den.com#g' {} \;
+find docs/* -name "*.html" -type f -exec sed -i '' 's#http://localhost:2368#https://nerds-den.com#g' {} \;
 
-sed -i '' '1s/^/<!DOCTYPE html><html lang="en"> /' static/index.html
-echo "</html>" >> static/index.html
+sed -i '' '1s/^/<!DOCTYPE html><html lang="en"> /' docs/index.html
+echo "</html>" >> docs/index.html
 
 # Add CNAME file for github pages
 buster add-domain nerds-den.com
 
 # Copy files that were missed by buster
-#cp humans.txt static/humans.txt
-cp -R content/images static/content
+#cp humans.txt docs/humans.txt
+cp -R content/images docs/content
