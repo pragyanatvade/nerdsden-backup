8cf582d20ba2312814b72dfde22c3ddd34c7050b
Pragyan Tripathi <pntripathi9417@gmail.com>
2018-11-21 08:42:02 +0530

    fix(nerdsden): replace http://localhost:2368 with https://nerds-den.com

--- build-static.sh
+++ build-static.sh
@@ -16,12 +16,8 @@ find static/* -name "robots.txt" -type f -exec sed -i '' 's#http://localhost:236
 find static/* -name "*.xsl"  -type f -exec sed -i '' 's#http://localhost:2368#https://nerds-den.com#g' {} \;
 find static/* -name "*.xml"  -type f -exec sed -i '' 's#href="//localhost:2368#href="//nerds-den.com#g' {} \;
 find static/* -name "*.xml"  -type f -exec sed -i '' 's#loc>http://localhost:2368#loc>https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#u=http://localhost:2368#u=https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#url=http://localhost:2368#url=https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#href="http://localhost:2368#href="https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#href="http://127.0.0.1:2368#href="https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#src="http://localhost:2368#src="https://nerds-den.com#g' {} \;
-find static/* -name "*.html" -type f -exec sed -i '' 's#link>http://localhost:2368#link>https://nerds-den.com#g' {} \;
+find static/* -name "*.html" -type f -exec sed -i '' 's#http://localhost:2368#https://nerds-den.com#g' {} \;
+
 sed -i '' '1s/^/<!DOCTYPE html><html lang="en"> /' static/index.html
 echo "</html>" >> static/index.html
 
