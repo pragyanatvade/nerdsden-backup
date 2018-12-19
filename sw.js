1ee07d596d611c29cd346671852e3c0d7bf6b053
Pragyan Tripathi <pntripathi9417@gmail.com>
2018-12-19 15:17:43 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-69771f7b14c20bc277a2.js"
+    "url": "webpack-runtime-47c00af2fdb0a6535cad.js"
   },
   {
     "url": "app-48e99e202e1f67624bd5.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9ebf987d5b41e335e479da59bdc27435"
+    "revision": "d90b6c0f0e73e918a7ab1034c0a080b2"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
