35f68eac947cc31e45325bd85922a1280c8b115d
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-01-05 17:28:39 +0530

    feat(nerdsden): add share buttons in post template

---
+++ src/components/Share/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { siteUrl, pathPrefix } from '../../content/meta/config'
+
+const Share = props => {
+  return <div>Share Button</div>
+}
+
+export default Share
