216591812ddb64d2368333c21a7bedb15259c2e8
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-01-19 11:16:44 +0530

    fix(components): add card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import styled from "styled-components";
-import { Link } from "gatsby";
-import Img from "gatsby-image";
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
 const Post = styled.li`
   position: relative;
@@ -11,11 +11,11 @@ const Post = styled.li`
   width: 100%;
   transition: background 0.2s;
   @media screen and (min-width: ${props => props.theme.responsive.small}) {
-    flex: ${props => (props.featured ? "0 0 100%" : "0 0 49%")};
+    flex: ${props => (props.featured ? '0 0 100%' : '0 0 49%')};
     margin: 0 0 2vw 0;
   }
   @media screen and (min-width: ${props => props.theme.responsive.medium}) {
-    flex: ${props => (props.featured ? "0 0 100%" : "0 0 32%")};
+    flex: ${props => (props.featured ? '0 0 100%' : '0 0 32%')};
   }
   &:hover {
     background: ${props => props.theme.colors.tertiary};
@@ -31,47 +31,52 @@ const Post = styled.li`
       height: 0;
       padding-bottom: 60%;
       @media screen and (min-width: ${props => props.theme.responsive.small}) {
-        padding-bottom: ${props => (props.featured ? "40%" : "60%")};
+        padding-bottom: ${props => (props.featured ? '40%' : '60%')};
       }
     }
   }
-`;
+`
 
 const Title = styled.h2`
   font-size: 1.5em;
   font-weight: 600;
   text-transform: capitalize;
   margin: 1rem 1rem 0.5rem 1rem;
-`;
+`
 
 const Date = styled.h3`
   margin: 0 1rem 1.5rem 1rem;
   color: gray;
-`;
+`
 
 const Summary = styled.p`
   margin: 0 1rem 1rem 1rem;
   line-height: 1.6;
-`;
+`
 
 const Card = ({ slug, cover, title, date, summary, excerpt, ...props }) => {
   const {
-    children: [{ fluid }]
-  } = cover;
+    children: [{ fluid }],
+  } = cover
   return (
     <Post featured={props.featured}>
       <Link to={`/${slug}/`}>
-        <Img fluid={fluid} backgroundColor={"#eeeeee"} />
+        <Img
+          fluid={fluid}
+          backgroundColor={'#eeeeee'}
+          alt={title}
+          title={title}
+        />
         <Title>{title}</Title>
         <Date>{date}</Date>
         <Summary
           dangerouslySetInnerHTML={{
-            __html: summary || excerpt
+            __html: summary || excerpt,
           }}
         />
       </Link>
     </Post>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
