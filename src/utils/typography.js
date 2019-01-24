dd00c7d9657cb902b9eda10b552338f128072475
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-01-24 09:09:17 +0530

    fix(typography): copied theme - need to update it according to the content

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,15 +1,96 @@
-import Typography from "typography"
-import parnassusTheme from 'typography-theme-parnassus'
+import Typography from 'typography'
+import gray from 'gray-percentage'
+import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants'
+import verticalRhythm from 'compass-vertical-rhythm'
 
-const typography = new Typography(parnassusTheme)
+import { name } from '../../package.json'
 
-//import lawtonTheme from 'typography-theme-lawton'
-//const typography = new Typography(lawtonTheme)
+const theme = {
+  title: name,
+  baseFontSize: '19px',
+  baseLineHeight: 1.58,
+  googleFonts: [
+    {
+      name: 'Roboto Slab',
+      styles: ['700'],
+    },
+    {
+      name: 'Roboto',
+      styles: ['400', '400i', '700'],
+    },
+  ],
+  headerFontFamily: ['Roboto Slab', 'sans-serif'],
+  bodyFontFamily: ['Roboto', 'serif'],
+  headerColor: 'hsla(0,0%,0%,0.9)',
+  bodyColor: 'hsla(0,0%,0%,0.73)',
+  headerWeight: 700,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => {
+    const linkColor = '#950451'
+    const vr = verticalRhythm({
+      baseFontSize: '17px',
+      baseLineHeight: '28px',
+    })
+    return {
+      a: {
+        color: linkColor,
+        textDecoration: 'none',
+        textShadow:
+          '.03em 0 #fff,-.03em 0 #fff,0 .03em #fff,0 -.03em #fff,.06em 0 #fff,-.06em 0 #fff,.09em 0 #fff,-.09em 0 #fff,.12em 0 #fff,-.12em 0 #fff,.15em 0 #fff,-.15em 0 #fff', // eslint-disable-line
+        backgroundImage: `none`, // eslint-disable-line
+      },
+      'a:hover,a:active': {
+        textShadow: 'none',
+        backgroundImage: 'none',
+      },
+      'h1,h2,h3,h4,h5,h6': {
+        marginTop: rhythm(2),
+      },
+      // children ol, ul
+      'li>ol,li>ul': {
+        marginLeft: '20px',
+        marginBottom: 0,
+      },
+      // Blockquote styles.
+      blockquote: {
+        ...scale(1 / 5),
+        borderLeft: `${rhythm(6 / 16)} solid ${linkColor}`,
+        color: gray(35),
+        paddingLeft: rhythm(10 / 16),
+        fontStyle: 'italic',
+        marginLeft: 0,
+        marginRight: 0,
+      },
+      'blockquote > :last-child': {
+        marginBottom: 0,
+      },
+      'blockquote cite': {
+        ...adjustFontSizeTo(options.baseFontSize),
+        color: options.bodyColor,
+        fontStyle: 'normal',
+        fontWeight: options.bodyWeight,
+      },
+      'blockquote cite:before': {
+        content: '"— "',
+      },
+      [MOBILE_MEDIA_QUERY]: {
+        html: {
+          ...vr.establishBaseline(),
+        },
+        blockquote: {
+          borderLeft: `${rhythm(3 / 16)} solid ${linkColor}`,
+          color: gray(41),
+          paddingLeft: rhythm(9 / 16),
+          fontStyle: 'italic',
+          marginLeft: rhythm(-3 / 4),
+          marginRight: 0,
+        },
+      },
+    }
+  },
+}
 
-//import kirkhamTheme from 'typography-theme-kirkham'
-//const typography = new Typography(kirkhamTheme)
-
-//import irvingTheme from 'typography-theme-irving'
-//const typography = new Typography(irvingTheme)
+const typography = new Typography(theme)
 
 export default typography
