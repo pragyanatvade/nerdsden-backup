af5684db824311bc33ad060f4b5a152f2dcc4297
MOHINI TRIPATHI <trishmohi01@gmail.com>
2019-04-24 02:04:48 +0530

    adding grid layout

---
+++ src/components/GridLayout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'GridLayout.css'
+const GridLayout = props => (
+  <container className="grid-container">
+  <header className="grid-header">
+  <menu/>
+  </header>
+  <main className="grid-body">
+  <footer/>
+  </container>
+
+)
+export default GridLayout
